5198756be6a3321bbbbdae615f73825abe900770
Villadiego Hoyos <d.villadiego.hoyos@accenture.com>
2021-07-12 14:17:29 -0500

    start repo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useState } from "react";
+import StarRating from "./StarRating";
+import ColorList from "./ColorList";
+import colorData from "./color-data.json";
+
+function App() {
+  const [colors, setColors] = useState(colorData);
+
+  return (
+    <div>
+      <StarRating />
+      <ColorList
+        colors={colors}
+        onRemoveColor={(id) => {
+          const newColors = colors.filter((color) => color.id !== id);
+          setColors(newColors);
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StarRating.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Star from "./Star";
+import { createArray } from "./utils";
+
+export default function StarRating({ totalStars = 5 }) {
+  const [selectedStar, setSelectedStar] = useState(3);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={i < selectedStar}
+          onSelect={() => setSelectedStar(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStar} of {totalStars} stars
+      </p>
+    </>
+  );
+}
